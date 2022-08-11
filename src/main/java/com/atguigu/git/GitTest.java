23ab77a0752eb041ad0afce1b814aa1181134094
Lyp <202080066@hdu.edu.cn>
2022-08-11 15:02:10 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hello git3-hot-fix2!");
         System.out.println("hello master!");
         System.out.println("hello master1!");
+        System.out.println("push test");
     }
 }
