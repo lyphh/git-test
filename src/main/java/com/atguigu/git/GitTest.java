e2a0a95383b85f057cb83ebe56dcc4eb7e59c04f
lyphh <88382079+lyphh@users.noreply.github.com>
2022-08-11 15:15:50 +0800

    pull test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -17,5 +17,6 @@ public class GitTest {
         System.out.println("hello master!");
         System.out.println("hello master1!");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
