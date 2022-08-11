ca4147960cc8db7f8b4064c9df60cefcea1c9ba8
Lyp <202080066@hdu.edu.cn>
2022-08-11 14:34:51 +0800

    hot-fix second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello git1!");
         System.out.println("hello git2!");
         System.out.println("hello git3-hot-fix!");
+        System.out.println("hello git3-hot-fix2!");
     }
 }
