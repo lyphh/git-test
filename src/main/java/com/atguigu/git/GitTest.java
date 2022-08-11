4c80e69fe7ad1fd1dfca3a2b096e99837ec4e66e
Lyp <202080066@hdu.edu.cn>
2022-08-11 14:11:58 +0800

    Lyp first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName: GitTest
+ * @Description //TODO $
+ * @Author LYP
+ * @Date: 2022/8/11 14:08
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git1!");
+    }
+}
